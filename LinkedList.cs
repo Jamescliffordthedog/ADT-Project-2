85f31751a00f4db822e4a2be448302d867bcac4c
Jamescliffordthedog <james@cliffordclan.co.uk>
2025-12-02 15:38:59 +0000

    Couple functions

--- LinkedList.cs
+++ LinkedList.cs
@@ -4,9 +4,57 @@ namespace ADT_Project_2
 {
     public class LinkedList
     {
+        public Node? Head { get; set; };
+        public int Count { get; set; };
+
         public LinkedList()
         {
+            Head = null;
+            Count = 0;
+        }
+
+        public void DisplayPlaylist()
+        {
+            Node current = Head;
+            int index = 1;
+
+            if (Head == null)
+            {
+                Console.WriteLine("This playlist has no songs");
+                return;
+            }
+
+            while (true)
+            {
+                Console.WriteLine(index + ". " + current.Data.displaySong() + "/n");
+                current = current.Next!;
+                index++;
+
+                if (current == Head)
+                    break;
+            }
+        }
+
+        public void Add(Song song)
+        {
+            Node currentNode = new Node(song);
+
+            if (Head == null)
+            {
+                Head = currentNode;
+                Head.Next = Head;
+                Head.Prev = Head;
+            }
+            else
+            {
+                Node tail = Head.prev;
+                Head.Prev = currentNode;
+                tail.Next = currentNode;
+                currentNode.Next = Head;
+                currentNode.Prev = tail;
+            }
+
+            Count++;
         }
     }
-
 }
